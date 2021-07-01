7bb11725ed1a61bbd6a6fd21f2d03add1c725a51
Oscar Catari Gutierrez <oscarcatari@outlook.es>
2021-07-01 14:49:32 +0100

    [FIX] Cors error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
+var server = require('https').createServer(app);
 
 var totalConnections = 0;
 
